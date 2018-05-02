bb705060d5c151924f4a65cdbcc375008d76b5ca
Andrew Redd <andrew.redd@hsc.utah.edu>
2018-05-02 13:31:30 -0600

    added more support for relative comments and started the structure of the class definitions to support the relative functions.

---
+++ R/pd_classes.R
@@ -0,0 +1,60 @@
+
+
+.class.defining.functions <- c('setClass', 'setRefClass', 'R6Class')
+is_pd_class_definition <- 
+function( id = pd$id
+        , pd = get('pd', parent.frame())
+        , funs = .class.defining.functions #< valid class defining functions 
+        ){
+    if (length(id)>1) return(sapply(id, is_pd_class_definition, pd=pd, funs=funs))
+    is_pd_symbol_call(pd, id) &&
+    text(get_pd_call_symbol_id(pd, id)) %in% funs
+}
+if(FALSE){#@test
+'setClass( "testClass"
+     , slots = c( x="numeric" #< the x field
+                , y="matrix"  #< the y field
+                )
+     )' %>% 
+    parse(text = .) %>%
+    get_parse_data() -> pd
+    
+    expect_true(is_pd_class_definition(pd, id = all_root_ids(pd)))
+}
+
+#' @internal
+pd_is_in_class_definition <- 
+function( id = pd$id
+        , pd = get('pd', parent.frame())
+        , funs = .class.defining.functions #< valid class defining functions 
+        ){
+    if (length(id) > 1) return(sapply(id, pd_is_in_class_definition, pd=pd, funs=funs))
+    ancestors <- get_ancestor_ids(pd, id, only.present=TRUE)
+    any(is_pd_class_definition(ancestors, pd=pd, funs=funs))
+}
+if(FALSE){#@test object in setClass
+'setClass( "testClass"
+     , slots = c( x="numeric" #< the x field
+                , y="matrix"  #< the y field
+                )
+     )
+setMethod("print", "testClass", function(){
+    cat("This is just a test.")
+})
+' %>% 
+    parse(text = .) %>%
+    get_parse_data() -> pd
+
+    root.id <- all_root_ids(pd)
+    
+    id <- pd[pd$text=="#< the x field", 'id']
+    
+    expect_true(pd_is_in_class_definition(id, pd))
+    
+    id2 <- pd[pd$text=='"This is just a test."', 'id']
+    expect_false(pd_is_in_class_definition(id2, pd))
+    
+    expect_identical(pd_is_in_class_definition(c(id, id2), pd), c(TRUE, FALSE))
+}
+
+
---
+++ R/pd_comments.R
@@ -0,0 +1,85 @@
+
+
+#' Associate relative ducumentation comments
+#' 
+#' Relative comment created with `#<` comment tags document something
+#' designated by the location of the comment.
+#' In general, the comment documents the previous symbol.
+#' A comment will not be associated with any parse id that does not have
+#' the same parent as the comment. For example,
+#' 
+#'     function(x #< a valid comment
+#'             ){
+#' 
+#' would associate `a valid comment` with `x`, but
+#' 
+#'     function(x){ #< not a valid comment     
+#' 
+#' would not.
+#' 
+#' @value Returns a vector of the same length as id.  Where the value is 
+#'        either the id of the associated object or NA if it cannot be 
+#'        associated.
+associate_relative_comments <- 
+function(pd, id = get_relative_comments(pd)$id){
+    if (length(id)>1L) return(sapply(id, associate_relative_comments, pd=pd))
+
+    sibs <- get_sibling_ids(pd, id)
+    possible <- sibs[token(sibs, pd) == 'SYMBOL_FORMALS']
+    if (length(possible) == 0L) return(NA)
+    possible <- possible[end_line(possible) <= start_line(id)]
+    if (length(possible)==1L) return(possible)
+    if (length(possible) == 0L) return(NA)
+    possible <- possible[end_line(possible) == max(end_line(possible))]
+    possible <- possible[end_col(possible) == max(end_col(possible))]
+    stopifnot(length(possible) == 1)
+    return(possible)
+}
+if(F){#@test function relative comments
+'function( pd                    #< parse data
+                                #< continuation comment
+        , id = all_root_ids(pd) #< id number
+        ){}' %>%
+    parse(text = .) %>%
+    get_parse_data() -> pd
+    
+    id <- get_relative_comments(pd)$id
+
+    value <- associate_relative_comments(pd)
+    expect_identical(value[[1]], value[[2]])
+    expect_identical(text(value, pd=pd), c('pd', 'pd', 'id'))
+
+# while one argument documented and another not should be discouraged, 
+# it is allowed.
+'function( pd, id = all_root_ids(pd) #< id number
+        ){}' %>%
+    parse(text = .) %>%
+    get_parse_data() -> pd
+    id <- get_relative_comments(pd)$id
+
+    expect_identical(text(associate_relative_comments(pd,id), pd=pd), 'id')
+
+'function( pd, #< traditional comma placement.
+           id = all_root_ids(pd) #< id number
+         ){}' %>%
+    parse(text = .) %>%
+    get_parse_data() -> pd
+    id <- get_relative_comments(pd)$id
+
+    value <- associate_relative_comments(pd,id)
+    expected <- pd[ token(pd=pd)  ==  "SYMBOL_FORMALS"
+                  & text(pd=pd)  %in% c("pd", "id")
+                  , 'id']
+    expect_identical(value, expected)
+}
+if(F){#@test class members
+'setClass( "testClass"
+         , slots = c( x="numeric" #< the x field
+                    , y="matrix"  #< the y field
+                    )
+         )' %>% 
+    parse(text = .) %>%
+    get_parse_data() -> pd
+
+    
+}
